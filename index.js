70e6694ac1606fa409aa4175d0f4a2835e27b210
Daniel Fleace <danielfleace15@gmail.com>
2022-05-26 21:27:52 -0400

    Deploy to Heroku

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/api/task", (req, res) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
